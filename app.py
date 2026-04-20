8a2a7f483f7ba45ea4a4e36ee68a953c1a0d7d6e
arun-biju-8 <arunbiju8304@gmail.com>
2026-04-20 12:16:21 +0530

    Backend Features are added

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+
+model = tf.keras.models.load_model("land_model.h5")
+
+classes = ["Barren", "Urban", "Vegetation", "Water"]
+
+def preprocess(img):
+    img = img.resize((128,128))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+@app.route("/")
+def home():
+    return "LandLens API Running 🚀"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files["image"]
+    img = Image.open(file).convert("RGB")
+
+    processed = preprocess(img)
+    pred = model.predict(processed)
+
+    result = classes[np.argmax(pred)]
+
+    return jsonify({"prediction": result})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
